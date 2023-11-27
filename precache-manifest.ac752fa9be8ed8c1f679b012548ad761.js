be6f804c54043e2ef7c2cdbab980874428370f7f
raxit1902 <rax1920@gmail.com>
2023-11-27 15:35:42 +0530

    Updates

---
+++ precache-manifest.ac752fa9be8ed8c1f679b012548ad761.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c83687dbdbf62bd2878156262a5a2873",
+    "url": "/assessment_task/index.html"
+  },
+  {
+    "revision": "80882a38f2ab6ab3dbde",
+    "url": "/assessment_task/static/css/main.2eb8f4b5.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/assessment_task/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/assessment_task/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "80882a38f2ab6ab3dbde",
+    "url": "/assessment_task/static/js/main.04775e39.chunk.js"
+  },
+  {
+    "revision": "a33d8161353bc744efc8",
+    "url": "/assessment_task/static/js/runtime~main.d1d260ed.js"
+  }
+]);
