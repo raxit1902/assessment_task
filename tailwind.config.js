b01cd7ab8bc03f405fe8ad2f3deb63849f431f98
raxit1902 <rax1920@gmail.com>
2023-11-28 11:32:15 +0530

    tailwind change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge:[
-    "./src/components/**/*.{js,jsx}",
-    "./public/index.html"
-  ],
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
